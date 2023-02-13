ab5860cec4ab729740f22bcd22a1d441e418611a
kostyafip <124047828+kostyafip@users.noreply.github.com>
2023-02-13 09:51:57 +0300

    Three stamps for renerating plain text
    These three stamps generate plain text.
    First stamp just gives a sequence of character with no structure at all
    Second stamp adds spaces in such sequence now and then
    Third stamp generate sequence of random words taken from dictionary

---
+++ blobstamper/stamp_text.cpp
@@ -0,0 +1,71 @@
+/******************************************************************************
+ *
+ * Copyright 2021 Nikolay Shaplov (Postgres Professional)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+#include"stamp_text.h"
+
+std::string
+StampTextPulp::ExtractStr(Blob &blob)
+{
+
+    std::vector<char> data = blob.ChopBlank(*this);
+
+    std::vector<char>::iterator the_iterator;
+
+    the_iterator = data.begin();
+    std:: string res;
+    while (the_iterator != data.end())
+    {
+      if (*the_iterator == '\0') { *the_iterator = ' '; }
+      res.push_back(*the_iterator++);
+    }
+
+    return res;
+}
+
+std::string StampTextPulpWords::ExtractStr(Blob &blob)
+{
+  std::vector<std::string> data = ExtractStrVector(blob);
+  std::string res = "";
+
+  for(std::string s : data)
+  {
+    if (!res.empty())
+    {
+      res+=" ";
+    }
+    res+= s;
+  }
+  return res;
+}
+
+std::string StampTextDictWords::ExtractStr(Blob &blob)
+{
+  std::vector<std::string> data = ExtractStrVector(blob);
+  std::string res = "";
+
+  for(std::string s : data)
+  {
+    if (!res.empty())
+    {
+      res+=" ";
+    }
+    res+= s;
+  }
+  return res;
+}
+
