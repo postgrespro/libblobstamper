15082f173c008df9d01632281ea110e370a8499f
Grigoriev Konstantin <k.grigorev@postgrespro.ru>
2023-02-13 09:51:57 +0300

    Three stamps for renerating plain text
    These three stamps generate plain text.
    First stamp just gives a sequence of character with no structure at all
    Second stamp adds spaces in such sequence now and then
    Third stamp generate sequence of random words taken from dictionary

---
+++ t/150-stamp_text.cpp
@@ -0,0 +1,60 @@
+/******************************************************************************
+ *
+ * Copyright 2021 Nikolay Shaplov (Postgres Professional)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+#include <string.h>
+
+#include <exception>
+#include <string>
+#include <cstdlib>
+#define WANT_TEST_EXTRAS
+#include <tap++/tap++.h>
+
+#include "blobstamper/stamp_text.h"
+
+using namespace TAP;
+
+int
+main()
+{
+    TEST_START(3);
+    { /* 1..1 */
+		  char data[] = "папа\0мама\0бабушка\0дедушка\0братик\0сестричка";
+		  Blob blob(data, (sizeof data)-1);
+		  StampTextPulp stamp;
+		  std::string s = stamp.ExtractStr(blob);
+      is(s, "папа мама бабушка дедушка братик сестричка", "StampTextSimple");
+
+    }
+    { /* 2..2 */
+		  char data[] = "dad\0mam\0granddad\0grandmam\0brother\0sister";
+		  Blob blob(data, (sizeof data)-1);
+		  StampTextPulpWords stamp;
+		  std::string s = stamp.ExtractStr(blob);
+      is(s, "d dad gra n dmam broth er siste", "GalleyTextSimple");
+
+    }
+    { /* 3..3 */
+		  char data[] = "abcdef" "abcdef" "ABCDEF" "012345";
+		  Blob blob(data, (sizeof data)-1);
+		  StampTextDictWords stamp;
+		  std::string s = stamp.ExtractStr(blob);
+      is(s, "gleam godfather graffiti greened grouping gunshots gleam godfather graffiti greened grouping gunshots dismally dissented divested doorstep dread drunks convertors corpulent counterparts cranking crippled crusades", "GalleyLCAlphaSmall");
+
+    }
+    TEST_END;
+}
